fe1be2cc7abb2b2f82d9c9858490278c8b1c655f
Roeich <roeichsheikh@gmail.com>
2024-11-23 22:24:02 +0600

    responsive without header navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
     /* ___________________ start home page ___________________ */
     
     // ... header secction
-    // $(".menu_toggleBtn,.header_backDrop").click(function() {
-    //     $(".header").toggleClass("active_header");
-    // });
+    $(".menu_toggleBtn,.header_backDrop").click(function() {
+        $(".header").toggleClass("active_header");
+    });
 
-    // function checkScroll() {
-    //     if ($(window).scrollTop() > 50) {
-    //         $("body").addClass('scrolled_header');
-    //     } else {
-    //         $("body").removeClass('scrolled_header');
-    //     }
-    // }
-    // checkScroll();
-    // $(window).on('scroll', function() {
-    //     checkScroll();
-    // });
+    function checkScroll() {
+        if ($(window).scrollTop() > 50) {
+            $("body").addClass('scrolled_header');
+        } else {
+            $("body").removeClass('scrolled_header');
+        }
+    }
+    checkScroll();
+    $(window).on('scroll', function() {
+        checkScroll();
+    });
     
     /* ___________________ end home page ___________________ */
 });
